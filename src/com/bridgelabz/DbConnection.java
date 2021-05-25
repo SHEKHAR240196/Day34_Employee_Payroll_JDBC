d070ce700204c253cd34a3f983e7d0186693b4ac
shekhar <shekharpatle999@gmail.com>
2021-05-25 08:20:48 +0530

    Added: code for a employeepayroll jdbc

---
+++ src/com/bridgelabz/DbConnection.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public interface DbConnection {
+	
+	String driverName = "com.mysql.cj.jdbc.Driver";
+	String url ="jdbc:mysql://localhost:3306/employee_payroll";
+	String userName ="root";
+	String password = "root";
+
+}
